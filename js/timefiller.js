9757430625266211740749fead0ef5fee2588fe7
Paul Kilgo <pkilgo@clemson.edu>
2013-10-30 15:44:58 -0400

    initial commit

---
+++ js/timefiller.js
@@ -0,0 +1,9 @@
+document.getElementById("day0_total").value = 0;
+document.getElementById("day1_total").value = 4;
+document.getElementById("day2_total").value = 4;
+document.getElementById("day3_total").value = 4;
+document.getElementById("day4_total").value = 4;
+document.getElementById("day5_total").value = 4;
+document.getElementById("day6_total").value = 0;
+document.getElementById("total_week").value = 20;
+document.getElementById("int_time").submit();
