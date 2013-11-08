a427b1eda7f0ca152bfa0887e929490de6e11a9e
Paul Kilgo <pkilgo@clemson.edu>
2013-11-08 14:12:52 -0500

    dont submit automatically

--- js/timefiller.js
+++ js/timefiller.js
@@ -6,4 +6,3 @@ document.getElementById("day4_total").value = 4;
 document.getElementById("day5_total").value = 4;
 document.getElementById("day6_total").value = 0;
 document.getElementById("total_week").value = 20;
-document.getElementById("int_time").submit();
